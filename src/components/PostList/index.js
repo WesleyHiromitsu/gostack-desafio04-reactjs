62f749a39fdb433c7ef79eff1ebd48260566451b
WesleyHiromitsu <wes.hiro@gmail.com>
2020-03-03 13:59:34 -0300

    Added Avatar and Content -> Post

--- src/components/PostList/index.js
+++ src/components/PostList/index.js
@@ -9,40 +9,51 @@ class PostList extends Component {
       {
         id: 1,
         author: {
-          name: "Julio Alcantara",
-          avatar: "https://robohash.org/23e9e630457b26bcecea8a996f2df4c3?set=set4&bgset=&size=400x400"
+          name: "Jake",
+          avatar: "https://www.urbanarts.com.br/imagens/produtos/119591/0/Ampliada/jake-minimalista.jpg"
         },
-        date: "04 Jun 2019",
-        content: "Pessoal, alguém sabe se a Rocketseat está contratando?",
+        date: "20 Feb 2020",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         comments: [
           {
             id: 2,
             author: {
-              name: "Diego Fernandes",
-              avatar: "https://gravatar.com/avatar/23e9e630457b26bcecea8a996f2df4c3?s=400&d=mp&r=x"
+              name: "Finn",
+              avatar: "https://comicvine1.cbsistatic.com/uploads/original/6/67956/2326456-Adventure-Time-adventure-time-with-finn-and-jake-25206525-540-720.jpg"
             },
-            content: "Conteúdo do comentário"
+            content: 
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac placerat vestibulum lectus mauris ultrices."
           }
         ]
       },
 
       {
-        id: 1,
+        id: 3,
         author: {
-          name: "Julio Alcantara",
-          avatar: "https://robohash.org/23e9e630457b26bcecea8a996f2df4c3?set=set4&bgset=&size=400x400"
+          name: "Princess Bubblegum",
+          avatar: "https://i.pinimg.com/originals/a1/1b/89/a11b89c2394023c89e877654beb20d32.jpg"
         },
-        date: "04 Jun 2019",
-        content: "Pessoal, alguém sabe se a Rocketseat está contratando?",
+        date: "22 Feb 2020",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         comments: [
+          {
+            id: 1,
+            author: {
+              name: "Jake",
+              avatar: "https://www.urbanarts.com.br/imagens/produtos/119591/0/Ampliada/jake-minimalista.jpg"
+            },
+            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac placerat vestibulum lectus mauris ultrices."
+          },
           {
             id: 2,
             author: {
-              name: "Diego Fernandes",
-              avatar: "https://gravatar.com/avatar/23e9e630457b26bcecea8a996f2df4c3?s=400&d=mp&r=x"
+              name: "Finn",
+              avatar: "https://comicvine1.cbsistatic.com/uploads/original/6/67956/2326456-Adventure-Time-adventure-time-with-finn-and-jake-25206525-540-720.jpg"
             },
-            content: "Conteúdo do comentário"
-          }
+            content: 
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium nibh ipsum consequat nisl vel pretium lectus quam id. Odio tempor orci dapibus ultrices."
+          },
+          
         ]
       },
 
